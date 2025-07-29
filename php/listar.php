e55d46cb1bb87449d0c0ab1470f5d857fb958217
Kaique <kaique1245br@gmail.com>
2025-07-29 08:27:01 -0300

    First commit

---
+++ php/listar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/listar.css">
+    <title>Listar pokemom</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="listar.php"></a>Listar</li>
+                <li><a href="cadastrar.php"></a>Cadastrar</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
