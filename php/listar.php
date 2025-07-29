0600bc86f607f5e33d7c45f51d065bd1945cab14
Kaique <kaique1245br@gmail.com>
2025-07-29 09:42:51 -0300

    Second commit

--- php/listar.php
+++ php/listar.php
@@ -12,13 +12,29 @@
     <header>
         <nav>
             <ul>
-                <li><a href="listar.php"></a>Listar</li>
-                <li><a href="cadastrar.php"></a>Cadastrar</li>
+                <li><a href="listar.php">Listar</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
             </ul>
         </nav>
     </header>
     <main>
-
+        <div class="container">
+            <div class="pesquisa">
+                <h1>Pesquisar Pokémom</h1>
+                <input type="search">
+            </div>
+            <div class="card_container">
+                <div class="card">
+                    <h1>Pikachu</h1>
+                    <p><strong>Tipo:</strong> Elétrico</p>
+                    <p><strong>Localização:</strong> Caçapava</p>
+                    <p><strong>Data do Registro:</strong> 29/07/2025</p>
+                    <p><strong>HP:</strong> 100</p>
+                    <p><strong>Ataque:</strong> 50</p>
+                    <p><strong>Defesa:</strong> 30</p>
+                </div>
+            </div>
+        </div>
     </main>
     <footer>
 
